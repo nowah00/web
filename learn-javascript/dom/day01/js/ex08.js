8ad2a995f715c8a1021bce40e82a11654d39ec9b
nowah00 <hawon2k@gmail.com>
2025-10-17 15:34:53 +0900

    10/17/15:34_commit

---
+++ learn-javascript/dom/day01/js/ex08.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  const header = document.getElementById('header');
+
+  header.style.border = '1px solid black';
+  header.style.color = 'red';
+};
---
+++ learn-javascript/dom/day01/js/ex5.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  // const header1 = document.querySelector('h1');
+  const header2 = this.document.getElementById('header');
+
+  header.innerHTML = '<i> i 태그입니다 </i><br/>.';
+  header.innerHTML += '&lti;&lt;i 태그입니다.&lti;&lt;';
+};
